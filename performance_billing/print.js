721473c68cc2064670f7d3a0ebb4cf7f82770f19
Ivan Bowen <66402923+874bowen@users.noreply.github.com>
2023-11-15 08:24:41 +0300

    use better naming convention add playFor function

--- performance_billing/print.js
+++ performance_billing/print.js
@@ -2,26 +2,30 @@ const invoices = require('./invoices.json');
 const plays = require('./plays.json');
 
 // amountFor(aPerformance)
-function amountFor(perf, play) {
+function amountFor(aPerformance, play) {
    let result = 0;
    switch (play.type) {
       case "tragedy":
          result = 40000;
-         if (perf.audience > 30) {
-            result += 1000 * (perf.audience, 30);
+         if (aPerformance.audience > 30) {
+            result += 1000 * (aPerformance.audience, 30);
          }
          break;
       case "comedy":
          result = 30000;
-         if (perf.audience > 20) {
-            result += 10000 + 500 * (perf.audience, 20);
+         if (aPerformance.audience > 20) {
+            result += 10000 + 500 * (aPerformance.audience, 20);
          }
-         result += 300 * perf.audience;
+         result += 300 * aPerformance.audience;
          break;
       default:
          throw new Error(`unknown type: ${play.type}`);
    }
    return result;
+}
+
+function playFor(aPerformance) {
+   return plays[aPerformance.playID];
 }
 
 function statement(invoice, plays) {
@@ -34,8 +38,7 @@ function statement(invoice, plays) {
       minimumFractionDigits: 2
    }).format;
    for (let perf of invoice.performances) {
-      let play = plays[perf.playID];
-      play = JSON.parse(JSON.stringify(play));
+      let play = playFor(perf);
       let thisAmount = amountFor(perf, play);
       // add volume credits
       volumeCredits += Math.max(perf.audience, 30, 0);
